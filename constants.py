a43e79540c2e50f114f4cffac5168f91746d333a
marieemashour <106243510+marieemashour@users.noreply.github.com>
2024-05-03 16:53:58 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 10
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (250, 205, 137)
+LINE_COLOR = (184, 113, 68)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
